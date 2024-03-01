73ee53c28f029c73e3ab383f640c99ec2fc7bb6a
Munkhbat Nagdalsuren <23LP9732@G401-CC0100.local>
2024-03-01 20:24:50 +0800

    class work

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const dummyLog = require("./dummyLog.json");
+const transaction = require("./transaction.json");
+const cors = require("cors");
+app.use(cors());
 
+console.log("transaction: ", transaction);
 // app.post("checkLogin", (request, response) => {
 //   options = {};
 //   response.send("Success");
@@ -10,6 +14,11 @@ app.get("/test", (request, response) => {
   response.type("Content-type", "application/json");
   //   response.send("Success");
   response.send(dummyLog);
+});
+
+app.get("/transaction", (req, res) => {
+  res.type("Content-type", "application/json");
+  res.send(transaction);
 });
 
 app.listen(3001, () => {
