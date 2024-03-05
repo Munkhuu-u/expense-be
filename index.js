99b7cd930ed47e93a5df33b48435928952484fc4
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-03-05 12:47:50 +0800

    add table failed

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const { Pool } = require("pg");
+// const { dotenv } = require("dotenv");
+require("dotenv").config();
 const dummyLog = require("./dummyLog.json");
 const transaction = require("./transaction.json");
-const cors = require("cors");
-app.use(cors());
 
-console.log("transaction: ", transaction);
-// app.post("checkLogin", (request, response) => {
-//   options = {};
-//   response.send("Success");
-// });
+const app = express();
+app.use(cors());
+// dotenv.config();
+// console.log("process.env :", process.env.PGDATABASE);
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
+// PGHOST="ep-flat-darkness-a5pb60l0.us-east-2.aws.neon.tech"
+// PGDATABASE="expenseDB"
+// PGUSER="munkhbat.nagdal"
+// PGPASSWORD="oW7LPJufatg2"
+// PGPORT=3001
+console.log("PGHOST: ", PGHOST);
+const pgConfig = {
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: PGPORT,
+  ssl: {
+    require: true,
+  },
+};
+
+const pool = new Pool(pgConfig);
+
+app.get("/add-user", async (req, res) => {
+  const client = await pool.connect();
+  console.log("client: ", client);
+  try {
+    client.query(
+      "CREATE TABLE users (name VARCHAR(255), age INT, phone VARCHAR(255), email VARCHAR(255))"
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.release();
+  }
+  res.status(200).send({ message: "add table working successfully" });
+});
+
 app.get("/test", (request, response) => {
   response.type("Content-type", "application/json");
-  //   response.send("Success");
   response.send(dummyLog);
 });
 
@@ -22,5 +57,5 @@ app.get("/transaction", (req, res) => {
 });
 
 app.listen(3001, () => {
-  console.log("Back End app listening at port 3001");
+  console.log("Back End app is listening at port 3001");
 });
