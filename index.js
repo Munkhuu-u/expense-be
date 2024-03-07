08491bf0bb77bec514a62818fe8563e16166b02a
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-03-07 12:45:06 +0800

    make log in work

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
-// const { dotenv } = require("dotenv");
 require("dotenv").config();
 const dummyLog = require("./dummyLog.json");
 const transaction = require("./transaction.json");
 
 const app = express();
 app.use(cors());
-// dotenv.config();
-// console.log("process.env :", process.env.PGDATABASE);
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
-// PGHOST="ep-flat-darkness-a5pb60l0.us-east-2.aws.neon.tech"
-// PGDATABASE="expenseDB"
-// PGUSER="munkhbat.nagdal"
-// PGPASSWORD="oW7LPJufatg2"
-// PGPORT=3001
+
 console.log("PGHOST: ", PGHOST);
 const pgConfig = {
   host: PGHOST,
@@ -31,7 +24,7 @@ const pgConfig = {
 
 const pool = new Pool(pgConfig);
 
-app.get("/add-user", async (req, res) => {
+app.get("/addUsersTable", async (req, res) => {
   const client = await pool.connect();
   console.log("client: ", client);
   try {
@@ -46,9 +39,18 @@ app.get("/add-user", async (req, res) => {
   res.status(200).send({ message: "add table working successfully" });
 });
 
-app.get("/test", (request, response) => {
-  response.type("Content-type", "application/json");
-  response.send(dummyLog);
+app.post("/addUser", async (req, res) => {
+  const user = req.body;
+  console.log("req.body in BE: ", req.body);
+  // try {
+  //   const client = await pool.connect();
+  //   client.query(`INSERT INTO users (age, email, name, phone) VALUES(${},,,)`);
+  // } catch (error) {
+  //   console.log(error);
+  // } finally {
+  // }
+  res.type("Content-type", "application/json");
+  res.send(transaction);
 });
 
 app.get("/transaction", (req, res) => {
