9ded5c86e69169d0f17ed9aef8d22ac29801f793
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-03-10 13:04:08 +0800

    can not get body request

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const app = express();
 app.use(cors());
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
-
-console.log("PGHOST: ", PGHOST);
-const pgConfig = {
+const pool = new Pool({
   host: PGHOST,
   database: PGDATABASE,
   username: PGUSER,
@@ -20,57 +18,57 @@ const pgConfig = {
   ssl: {
     require: true,
   },
-};
-
-const pool = new Pool(pgConfig);
-
-app.get("/addUsersTable", async (req, res) => {
-  const client = await pool.connect();
-  console.log("client: ", client);
-  try {
-    client.query(
-      "CREATE TABLE users (name VARCHAR(255), age INT, phone VARCHAR(255), email VARCHAR(255))"
-    );
-  } catch (error) {
-    console.log(error);
-  } finally {
-    client.release();
-  }
-  res.status(200).send({ message: "add table working successfully" });
-});
-
-app.get("/addColumns", async (req, res) => {
-  const client = await pool.connect();
-  try {
-    client.query(
-      "ALTER TABLE users ADD id VARCHAR(255), ADD password VARCHAR(255), ADD createdAT VARCHAR(255), ADD currency_type VARCHAR(255)"
-    );
-  } catch (err) {
-    console.log(err);
-  } finally {
-    client.release();
-  }
 });
 
 app.post("/addUser", async (req, res) => {
-  const user = req.body;
-  console.log("req.body in BE: ", req.body);
-  // try {
-  //   const client = await pool.connect();
-  //   client.query(`INSERT INTO users (age, email, name, phone) VALUES(${},,,)`);
-  // } catch (error) {
-  //   console.log(error);
-  // } finally {
-  // }
-  res.type("Content-type", "application/json");
-  res.send(transaction);
-});
+  // app.post("/addUser", async (req, res) => {
+  console.log("req.body: ", req.body);
 
-app.get("/transaction", (req, res) => {
-  res.type("Content-type", "application/json");
-  res.send(transaction);
+  // const client = await pool.connect();
+  // try {
+  //   client.query(
+  //     `INSERT INTO users (name, password, email) VALUES ('MUNKHBAT', 'MKpassword', 'MKmail@gmail.com')`
+  //   );
+  // } catch (err) {
+  //   console.log(err);
+  // } finally {
+  //   client.release();
+  // }
+  res.status(200).send("added user successfully");
 });
 
 app.listen(3001, () => {
   console.log("Back End app is listening at port 3001");
 });
+
+//
+//
+// HERE IS E-P TO ADD TABLE
+// app.get("/addTable", async (req, res) => {
+//   const client = await pool.connect();
+//   try {
+//     client.query(
+//       "CREATE TABLE category (id VARCHAR(100), name VARCHAR(100), description VARCHAR(300), createdAt TIMESTAMP(2), updatedAt TIMESTAMP(2), category_image VARCHAR(300))"
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     client.release();
+//   }
+//   res.status(200).send({ message: "add table working successfully" });
+// });
+
+// HERE IS E-P TO ADD COLUMN
+// app.get("/addColumns", async (req, res) => {
+//   const client = await pool.connect();
+//   try {
+//     client.query(
+//       "ALTER TABLE users ADD id VARCHAR(255), ADD password VARCHAR(255), ADD createdAT VARCHAR(255), ADD currency_type VARCHAR(255)"
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   } finally {
+//     client.release();
+//   }
+//   res.status(200).send({ message: "add col working successfully" });
+// });
